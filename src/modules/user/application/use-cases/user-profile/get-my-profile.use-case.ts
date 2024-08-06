36652edb48c61570ecef120c007a787f07ef3fa4
Anton Lebedev <=>
2024-08-06 12:42:16 +0300

    short code

--- src/modules/user/application/use-cases/user-profile/get-my-profile.use-case.ts
+++ src/modules/user/application/use-cases/user-profile/get-my-profile.use-case.ts
@@ -1,7 +1,5 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
-import { CommonResolverInterface } from '../../../../../common/interfaces/common-resolver.interface';
 import { UserProfileEntityInterface } from '../../../domain/entity-interfaces/user-profile.entity-interface';
-import { UserModel } from '../../../infrastructure/database/models/user.model';
 import { UserRepository } from '../../../infrastructure/database/repositories/user.repository';
 import { UserProfileResolver } from '../../resolvers/user-profile.resolver';
 
@@ -10,10 +8,7 @@ export class GetMyProfileUseCase {
   @Inject(UserRepository)
   private readonly userRepository: UserRepository;
   @Inject(UserProfileResolver)
-  private readonly userProfileResolver: CommonResolverInterface<
-    UserModel,
-    UserProfileEntityInterface
-  >;
+  private readonly userProfileResolver: UserProfileResolver;
 
   public async exec(userId: number): Promise<UserProfileEntityInterface> {
     const user = await this.userRepository.getById(userId);
